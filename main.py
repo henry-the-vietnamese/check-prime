317916bd59c03de3f5c0a3413c5dae37f9982509
henry-the-vietnamese <henryfromvietnam@gmail.com>
2021-09-30 23:27:53 +0700

    add three functions

---
+++ main.py
@@ -0,0 +1,48 @@
+#
+# File:         game_body.py
+# Author:       Tan Duc Mai
+# Email:        tan.duc.work@gmail.com
+# Date:         12/8/2021
+# Description:  Three different functions to check if a number is prime.
+#   I hereby declare that I completed this work without any improper help
+#   from a third party and without using any aids other than those cited.
+#
+
+"""
+Three different functions to check whether a given number is a prime.
+Return True if it is a prime, False otherwise.
+Those three functions, from a to c, decreases in efficiency (takes longer time).
+"""
+
+
+def is_prime_a(n):
+    from math import sqrt
+    if n < 2:
+        return False
+    sqrt_n = int(sqrt(n))
+    for i in range(2, sqrt_n + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def is_prime_b(n):
+    if n > 1:
+        if n == 2:
+            return True
+        else:
+            for i in range(2, n):
+                if n % i == 0:
+                    return False
+            return True
+    return False
+
+
+def is_prime_c(n):
+    divisible = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            divisible += 1
+    if divisible == 2:
+        return True
+    return False
